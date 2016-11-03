676ac180ffda18b85ae174505bc8bb2006d0f082
hudbuz <hzb2@cornell.edu>
2016-11-03 14:11:42 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-const app = "I don't do much.";
+const digitalClock = {
+  time: Math.round(new Date().getTime()/1000),
+  startTicking: function() {
+    time = Math.round(new Date().getTime()/1000
+
+  }
+}

a5500470f9f47adf72a788caf01e5c2129d93ccd
hudbuz <hzb2@cornell.edu>
2016-11-03 15:00:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 const digitalClock = {
   time: Math.round(new Date().getTime()/1000),
   startTicking: function() {
-    time = Math.round(new Date().getTime()/1000
+    setInterval(() => this.time ++, 1000)
 
   }
 }
+
+
+var censor = function(word,string) {
+  return string.split(word).join('BLEEP')
+
+
+}
+
+const violenceCensor = censor.bind(null, 'violence')
+
+const drugsCensor = censor.bind(null, 'drugs')

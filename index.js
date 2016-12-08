96e785157a70836ec292761feb6f4da75e293532
Jake Brady <jake@cmdrconsulting.com>
2016-12-08 14:16:59 -0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much.";
+var digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function(){
+    setInterval(() => this.time++, 1000);
+  }
+};
+
+function censor(word, str){
+  let match = new RegExp(word, 'gi');
+  return str.replace(match, 'BLEEP');
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');

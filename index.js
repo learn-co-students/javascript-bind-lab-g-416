c4d0778486e2a1849f1c0b18510914a08938bd29
andrew <github email address>
2017-01-02 10:21:01 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much.";
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function () {
+    setInterval(() => this.time++, 1000);
+  }
+};
+
+function censor(censoredWord, input) {
+  const matcher = new RegExp(censoredWord, 'gi');
+  return input.replace(matcher, 'BLEEP');
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');

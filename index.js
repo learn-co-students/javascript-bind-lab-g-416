da3b3201b4264d6454cb93e7d01bc3cc415152ef
ndaniele <n.daniele@gmail.com>
2017-01-07 16:33:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much.";
+//const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now()/1000),
+  startTicking: function() {
+    setInterval(() => this.time++, 1000);
+  }
+};
+
+function censor(censoredWord, input) {
+  const matcher = new RegEx(censoredWord, 'gi');
+  return input.replace(matcher, 'BLEEP');
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');

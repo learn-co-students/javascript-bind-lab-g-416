c05eab0a88b40f64014e74717ba6ee3e47ff712a
michaelsimon <michael.simon@gmail.com>
2017-03-04 20:56:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-const app = "I don't do much.";
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function() {
+    var digitalClock = this;
+    setInterval(function(){
+          digitalClock.time++;
+    }, 1000);
+
+  }
+}
+
+const censor = (word, str) => { return str.split(word).join('BLEEP')}
+
+const violenceCensor = censor.bind(null, 'violence');
+
+const drugsCensor = censor.bind(null, 'drugs');

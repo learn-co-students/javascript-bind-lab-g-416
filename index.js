f3a2a57c03015c44a146108ba0cb3bc70ec1baa7
Caleb Faught <faughtcj@gmail.com>
2017-03-22 11:40:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking() {
+    setInterval(() => {
+      this.time++;
+    },1000);
+  },
+};
+
+function censor(word, string) {
+  var regex = new RegExp(word, 'g');
+  return string.replace(regex, 'BLEEP')
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+
+const drugsCensor = censor.bind(null, 'drugs');

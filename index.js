b063fd41cfdf38ea6f7af471e3ee2ef22e73d8c6
CJ Horton <thecjhorton@gmail.com>
2017-05-07 13:23:49 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(new Date().getTime() / 1000),
+  startTicking: function () {
+    setInterval(() => this.time++, 1000)
+  }
+}
+
+const censor = (word, string) => {
+  let matcher = new RegExp(word, "gi")
+  return string.replace(matcher, "BLEEP")
+}
+
+const violenceCensor = censor.bind(null, "violence");
+const drugsCensor = censor.bind(null, "drugs");

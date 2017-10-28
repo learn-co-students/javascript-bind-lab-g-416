0654038d5393296c0e7bed7c83013e7c9b97ee93
Samuel Lair <lair001@gmail.com>
2017-10-28 22:49:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function () {
+      setInterval(() => this.time++, 1000)
+      }
+};
+
+const censor = function(word, string) {
+  const censorRegExp = new RegExp(word, 'gi');
+  return string.replace(censorRegExp, 'BLEEP');
+};
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');

c4cff9c158c1317a040277684fb550fc6aa284fe
alecgalba <alecgalba@gmail.com>
2018-01-22 16:03:35 -0500

    got em

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function () {setInterval(() => this.time++, 1000)}
+}
+
+
+function censor(word, string) {
+  var word = new RegExp(word, 'g'); //'g' for global regexp finding
+  return string.replace(word, 'BLEEP')
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');

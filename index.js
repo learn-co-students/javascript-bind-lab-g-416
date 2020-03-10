51365de3dc34f26a0fc8e7c77f283a97fd3669ab
Lois <lois.lee@me.com>
2020-03-10 15:17:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+let digitalClock = {
+    time: Math.round(Date.now() / 1000),
+    startTicking: function() {
+      setInterval(() => this.time++, 1000);
+    }
+  };
+  
+  function censor(word, string) {
+    let re = new RegExp(word, "g")
+    return string.replace(re, 'BLEEP')
+  }
+  
+  const violenceCensor = censor.bind(null, 'violence');
+  const drugsCensor = censor.bind(null, 'drugs');

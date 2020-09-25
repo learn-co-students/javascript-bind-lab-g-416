fff7c4c850d79b014df2ced96c24f6fde18399b5
Tracy Ngo <tngo1206@gmail.com>
2020-09-25 16:40:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 const app = "I don't do much.";
+
+let digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function () {
+    setInterval(() => this.time++, 1000);
+  }
+}
+
+function censor(word, string) {
+  const matcher = new RegExp(word, 'gi');
+  return string.replace(matcher, 'BLEEP');
+}
+
+const violenceCensor = censor.bind(null, 'violence');
+const drugsCensor = censor.bind(null, 'drugs');

17b1217db87b67fae451274cd059b17d5dd79da7
ashok997 <ashok997@gmail.com>
2020-09-24 21:50:01 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-const app = "I don't do much.";
+let digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking() {
+    setInterval(() => this.time++, 1000);
+  },
+};
+
+const censor = (word, stringSample) => {
+  let regex = new RegExp(word, "gi");
+  return stringSample.replace(regex, "BLEEP");
+};
+
+const violenceCensor = (stringSample) => censor("violence", stringSample);
+const drugsCensor = (stringSample) => censor("drugs", stringSample);

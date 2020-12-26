e2eb926bc0d1430d8eb94531d56795cd60d8c4dd
rayalva407 <rayalva407@gmail.com>
2020-12-26 12:09:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function () {
+    setInterval(() => this.time++, 1000)
+  }
+}
+
+const censor = (word, string) => {
+  const matcher = new RegExp(word, 'gi');
+  return string.replace(matcher, 'BLEEP')
+}
+
+const violenceCensor = censor.bind(null, 'violence')
+
+const drugsCensor = censor.bind(null, 'drugs')

f42a0e4d26728b72267c549f6a3dc70eda70f8b5
github username <github email address>
2021-02-16 20:33:56 -0800

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let digitalClock={time:(Math.round(Date.now() / 1000)), startTicking: function(){
-    
+let digitalClock={time:(Math.round(Date.now() / 1000)), startTicking: function(t){
+    setInterval(this.time+1,1000).bind()
 }};
 

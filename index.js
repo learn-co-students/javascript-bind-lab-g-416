f86ef08090b59f7837d87b6308ba8675b72ac1c0
github username <github email address>
2021-02-23 10:14:28 -0800

    Done.

--- index.js
+++ index.js
@@ -2,15 +2,12 @@ let digitalClock={time:(Math.round(Date.now() / 1000)), startTicking: function(t
     setInterval(this.time+1,1000).bind(this)
 }};
 
- let censor=((word,string)=>{return string.split(word).join("BLEEP")})
+ function censor(word,string){
+     return string.split(word).join("BLEEP")
+    }
 
-function violenceCensor(string){
-   return censor("violence",string)
-}
-
-
-function drugsCensor(string){
-   return censor("drugs",string)
+    const violenceCensor = censor.bind(null, 'violence');
+    const drugsCensor = censor.bind(null, 'drugs');
 }
 
 
